1e304ae7377a7d5b4ae978021516f9b1f5c2918e
Simon Struck <simonstruck70@gmail.com>
2017-11-03 17:25:35 +0100

    Added private file to gitignore

--- upload.sh
+++ upload.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-scp _working_directory/modules/telegrambots.framework.NaNoWriMoTracker-1.0-SNAPSHOT-all.jar titan:telegram-framework/modules/
+scp _working_directory/modules/telegrambots.framework.NaNoWriMoTracker-*.jar titan:telegram-framework/modules/
